493b3e3228b5e895ee994105b63b163efb51e07c
Filipe Dias <filipejtdias@gmail.com>
2020-02-15 13:52:04 +0000

    Update preferences for source tree

--- src/os/preferences/macos/source_tree.sh
+++ src/os/preferences/macos/source_tree.sh
@@ -9,3 +9,9 @@ print_in_purple "\n   Source Tree\n\n"
 
 execute "defaults write com.torusknot.SourceTreeNotMAS allowForcePush -int 1" \
 	"Allow Force Push"
+
+execute "defaults write com.torusknot.SourceTreeNotMAS gitFetchPrune -int 1" \
+	"Prunce tracking branches no longer present on remotes"
+
+execute "defaults write com.torusknot.SourceTreeNotMAS useFixedWithCommitFont -int 1" \
+	"Use fixed-width font for commit messages"
